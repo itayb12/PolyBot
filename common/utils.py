4bb9e2e967abf0e9d7ce825ce6ceba7b4c888f9a
zoharn007 <zphar.nagar.msn@gmail.com>
2022-11-09 18:44:01 +0200

    add botDeploy.Jenkinsfile add botDeploy.yaml update Jenkinsfile add prepare_ansible_inv.py

--- common/utils.py
+++ common/utils.py
@@ -3,7 +3,7 @@ from yt_dlp import YoutubeDL
 
 def search_download_youtube_video(video_name, num_results=1):
     """
-    This function downloads the first num_results search results from Youtube
+    This function downloads the first num_results search results from Youtubee
     :param video_name: string of the video name
     :param num_results: integer representing how many videos to download
     :return: list of paths to your downloaded video files
